90731426b7153843f9a4ab56fbbb555486e3e910
Priyanka Pawar <priyankapawar1304@gmail.com>
2023-06-17 22:43:56 +0530

    first commit

---
+++ src/StringsAreEqualOrNot.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class StringsAreEqualOrNot {
+//    public static void main(String[] args){
+//        String name1="pinky";
+//        String name2="Pinky";
+//        if(name1.compareTo(name2)==0){
+//            System.out.println("Strings are equal");
+//
+//        }
+//        else{
+//            System.out.println("not equal");
+//        }
+//    }
+
+
+    //another way to check that the strings are equal or not is as follows:
+    public static void main(String[] args){
+        String name1="priya";
+        String name2="pinky";
+        if(name1.equals(name2)){
+            System.out.println("strings are equal");
+        }
+        else{
+            System.out.println("not equal");
+        }
+    }
+}
